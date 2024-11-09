ba29baa598836315ecd2f149b5a67a63dbd798b0
Astra321 <50966736+Astra321@users.noreply.github.com>
2024-11-09 13:33:31 +0200

    Update edgestore.ts

--- lib/edgestore.ts
+++ lib/edgestore.ts
@@ -1,6 +1,1 @@
-"use client";
-import { type EdgeStoreRouter } from "../app/api/edgestore/[...edgestore]/route";
-import { createEdgeStoreProvider } from "@edgestore/react";
-const { EdgeStoreProvider, useEdgeStore } =
-  createEdgeStoreProvider<EdgeStoreRouter>();
-export { EdgeStoreProvider, useEdgeStore };
+
